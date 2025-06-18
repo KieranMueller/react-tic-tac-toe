0071b2c4a56f63ddf77d0c37ece3576d0e54d9f0
Kieran Mueller (OSV) <kieran.mueller.osv@fedex.com>
2025-06-18 10:26:03 -0500

    css stuff

--- src/components/GameTile.tsx
+++ src/components/GameTile.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+
 export default function GameTile({status, handleClick, row, column}: 
     {status: null | 'X' | 'O', handleClick: any, row: number, column: number}) {
+    
+    let [rando, setRando] = useState<number>(0)
+
+    function handleClickHere() {
+        setRando(Math.floor(Math.random() * 2))
+        handleClick(row, column)
+    }
+    
     return (
         <div onClick={() => {
             if (!status) {
-                handleClick(row, column)
+                handleClickHere()
             }
-        }} className="tile">{status}</div>
+        }} className='tile'>
+            <p className={`${status ? rando === 1 ? 'slam-in' : 'spin' : ''}`}>{status}</p>
+        </div>
     )
 }
